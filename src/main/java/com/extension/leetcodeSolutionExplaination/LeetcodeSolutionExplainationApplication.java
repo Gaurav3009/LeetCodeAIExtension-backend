9b51d5e0118d7f54f07e1e60060b41fdba4834a9
Gaurav3009 <onlyprofessionalmails@gmail.com>
2026-03-20 13:00:27 +0530

    initial commit 2

---
+++ src/main/java/com/extension/leetcodeSolutionExplaination/LeetcodeSolutionExplainationApplication.java
@@ -0,0 +1,13 @@
+package com.extension.leetcodeSolutionExplaination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeSolutionExplainationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetcodeSolutionExplainationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/extension/leetcodeSolutionExplaination/LeetcodeSolutionExplainationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.extension.leetcodeSolutionExplaination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeSolutionExplainationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
